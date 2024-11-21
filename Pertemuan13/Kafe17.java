09ed4fb5093b127dc19cfcee4d789b89d2f3acb3
daffifiros <daffikun10@gmail.com>
2024-11-21 16:34:59 +0700

    Percobaan 1

---
+++ Pertemuan13/Kafe17.java
@@ -0,0 +1,19 @@
+package Pertemuan13;
+
+public class Kafe17 {
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam    -> Rp 15,000");
+        System.out.println("2. Cappucino     -> Rp 18,000");
+        System.out.println("3. Latte         -> Rp 22,000");
+        System.out.println("4. Teh Tarik     -> Rp 10,000");
+        System.out.println("5. Roti Bakar    -> Rp 15,000");
+        System.out.println("6. Mie Goreng    -> Rp 18,000");
+        System.out.println("===========================");
+        System.out.print("Silakan pilih menu yang Anda inginkan. ");
+    }
+
+    public static void main(String[] args) {
+        Menu();
+    }
+}
