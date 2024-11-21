7a23c68948e3f752ca3eae2756970ed909b308cf
daffifiros <daffikun10@gmail.com>
2024-11-21 23:09:43 +0700

    Percobaan 3

--- Pertemuan13/Kafe17.java
+++ Pertemuan13/Kafe17.java
@@ -24,7 +24,6 @@ public class Kafe17 {
     public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
         int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        
         return hargaTotal;
     }
 
